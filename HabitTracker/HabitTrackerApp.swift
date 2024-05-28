f613e5fe76a9c240d5fd52af2dcc9ab784b9f1ff
Om Preetham <ompreethambandi@gmail.com>
2024-05-28 14:20:28 -0500

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Om Preetham Bandi on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
